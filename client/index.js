eee5798c95be11abbfbd95c0d63254d2581459b9
Mia Baz <themiabaz@gmail.com>
2019-08-15 18:59:58 -0400

    add client/index.js react render

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+      <h1>Welcome to Bearabull Trading</h1>
+      <h2>simple portfolio management</h2>
+  </div>,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
