4b5fe59e15ca9566a792cc9d9ca7b015c221d137
Mia Baz <themiabaz@gmail.com>
2019-08-16 12:09:58 -0400

    redux works!

--- client/index.js
+++ client/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
+  <Provider store={store}>
   <div>
       <h1>Welcome to Bearabull Trading</h1>
       <h2>painless portfolio management</h2>
-  </div>,
+  </div>
+  </Provider>,
   document.getElementById('app')
 );
