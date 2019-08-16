0abab7f8a87248429024deed03ddbf7ea60d7b91
Mia Baz <themiabaz@gmail.com>
2019-08-16 11:55:04 -0400

    webpack now serving client/index.js

--- client/index.js
+++ client/index.js
@@ -6,5 +6,5 @@ ReactDOM.render(
       <h1>Welcome to Bearabull Trading</h1>
       <h2>simple portfolio management</h2>
   </div>,
-  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+  document.getElementById('app')
 );
