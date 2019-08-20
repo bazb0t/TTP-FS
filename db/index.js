6cb5c0f82d48ccf31d9497a9af3e3e26bb0a1126
Mia Baz <themiabaz@gmail.com>
2019-08-19 20:07:24 -0400

    added Sequelize associations; closes #25

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 const db = require('./db');
 const User = require('./UserModel');
-// additional models here
+const Transaction = require('./TransactionModel');
 
-// associations below
+// Associations
+User.hasMany(Transaction);
+Transaction.belongsTo(User);
 
 module.exports = db;
